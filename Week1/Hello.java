d744dc4311653bf7eeea46d0a92406b57c4a4017
anish2500 <tmganish2500@gmail.com>
2024-04-11 10:38:04 +0545

    new change task 2

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -3,7 +3,16 @@ public class Hello {
         /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("HelloWorld");
         System.out.print("This is my first program\n");
+        /*2. Extend/Modify the above program to print two additional lines:
+
+        This is my first program
+
+        I am on module ST4003CEM
+
+        System.out.print (not println) and produces the same output. */
         System.out.print(" I am on module -ST-4003CEM");
+
+        
     }
 
 }

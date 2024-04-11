57c9907dd6e0cb4f40f05037a529c3c1444393c2
anish2500 <tmganish2500@gmail.com>
2024-04-11 10:54:11 +0545

    task4 change

--- Week1/Task4.java
+++ Week1/Task4.java
@@ -1,5 +1,14 @@
 public class Task4 {
     public static void main(String[] args) {
+        /*4. WAP that prints the following pattern:
+
+    *
+
+    **
+
+    ***
+
+    **** */
         System.out.println("*\n**\n***\n****");
 
     }

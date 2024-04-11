dd6257623eb812aa71c12354e685724900a1bdca
anish2500 <tmganish2500@gmail.com>
2024-04-11 10:30:41 +0545

    Week 1 Task 2

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
     public static void main (String[] args) {
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("HelloWorld");
+        System.out.print("This is my first program\n");
+        System.out.print(" I am on module -ST-4003CEM");
     }
 
 }
+
